760de2611bafccbc1b4d4c815e1ae38464cbdb53
ahmet ugur <ahmet765ugur@gmail.com>
2015-10-20 17:18:08 +0300

    Subcategory logic changed, arranging product view

--- bluemart/Common/ViewCells/CategoryCell.cs
+++ bluemart/Common/ViewCells/CategoryCell.cs
@@ -13,11 +13,13 @@ namespace bluemart.Common.ViewCells
 	{	
 		Category mCategory;
 		List<Category> mCategoryList;
+		Dictionary<string, List<Product>> mProductDictionary;
 
 		public CategoryCell (StackLayout parentGrid, Category category, BrowseCategoriesPage parent = null, BrowseSubCategoriesPage subParent = null)
 		{
 			mCategory = category;
 			mCategoryList = new List<Category> ();
+			mProductDictionary = new Dictionary<string, List<Product>> ();
 
 			Grid mainCellGrid = new Grid (){VerticalOptions = LayoutOptions.Fill, HorizontalOptions = LayoutOptions.FillAndExpand, BackgroundColor = Color.Gray, Padding = 0, RowSpacing = 0 };
 
@@ -34,21 +36,7 @@ namespace bluemart.Common.ViewCells
 
 				categoryImage.Opacity = 0.5f;
 				await Task.Delay (200);
-				//IF category doesn't have a sub category
-				if( category.SubCategoriesList == null )
-				{
-					LoadProductsFromSubCategoriesPage(category.CategoryID,subParent);
-				}
-				else 
-				{
-					if( category.SubCategoriesList.Count == 0 )
-					{
-						LoadProductsPage(category.CategoryID,parent);							
-					}
-					else
-						LoadCategoriesPage(parent);
-				}
-				//category.Name
+				LoadProductsPage(category.CategoryID,parent);
 				categoryImage.Opacity = 1f;
 			};
 
@@ -69,55 +57,50 @@ namespace bluemart.Common.ViewCells
 
 		void LoadProductsPage(string categoryID,BrowseCategoriesPage parent)
 		{			
-			//this.ParentView.cl
-			parent.Navigation.PushAsync (new BrowseProductsPage (PopulateProducts (categoryID),mCategory));
+			PopulateProducts ();
+			parent.Navigation.PushAsync (new BrowseProductsPage (mProductDictionary,mCategory));
 		}
 
-		void LoadProductsFromSubCategoriesPage(string categoryID,BrowseSubCategoriesPage parent)
-		{			
-			parent.Navigation.PushAsync (new BrowseProductsPage (PopulateProducts (categoryID),mCategory));
-		}
-
-		void LoadCategoriesPage( BrowseCategoriesPage parent)
+		private void PopulateProducts()
 		{
+			mProductDictionary.Clear ();
 			PopulateCategories ();
-			parent.Navigation.PushAsync (new BrowseSubCategoriesPage (mCategoryList,mCategory));
+
+			foreach( Category category in mCategoryList )
+			{
+				List<Product> product = new List<Product> ();
+
+				if (ProductModel.mProductCategoryIDDictionary.ContainsKey (category.CategoryID)) {
+					foreach (string productID in ProductModel.mProductCategoryIDDictionary[category.CategoryID]) {
+						string ImagePath = ProductModel.mRootFolderPath + "/" + ParseConstants.IMAGE_FOLDER_NAME + "/" + ProductModel.mProductImageNameDictionary [productID] + ".jpg";
+						string ProductName = ProductModel.mProductNameDictionary [productID];
+						double price = ProductModel.mProductPriceDictionary [productID];
+						string quantity = ProductModel.mProductQuantityDictionary [productID];
+						product.Add (new Product (productID, ProductName, ImagePath, price, quantity)); 
+					}
+				}
+
+				mProductDictionary.Add (category.Name, product);
+			}
 		}
 
 		void PopulateCategories()
 		{
 			mCategoryList.Clear ();
 
-			if(mCategory.CategoryModel.mSubCategoryDictionary.ContainsKey( mCategory.CategoryID ) )
-			{
-				foreach (string subCategoryID in mCategory.CategoryModel.mSubCategoryDictionary[mCategory.CategoryID] ){
-					string ImagePath = mCategory.CategoryModel.mRootFolderPath + "/" + ParseConstants.IMAGE_FOLDER_NAME + "/" + mCategory.CategoryModel.mImageNameDictionary[subCategoryID] + ".jpg";
+			if (mCategory.CategoryModel.mSubCategoryDictionary.ContainsKey (mCategory.CategoryID) && 
+				mCategory.CategoryModel.mSubCategoryDictionary[mCategory.CategoryID].Count > 0) {
+				foreach (string subCategoryID in mCategory.CategoryModel.mSubCategoryDictionary[mCategory.CategoryID]) {
+					string ImagePath = mCategory.CategoryModel.mRootFolderPath + "/" + ParseConstants.IMAGE_FOLDER_NAME + "/" + mCategory.CategoryModel.mImageNameDictionary [subCategoryID] + ".jpg";
 					string CategoryName = mCategory.CategoryModel.mCategoryNameDictionary [subCategoryID];
 					List<string> SubCategoryIDList = mCategory.CategoryModel.mSubCategoryDictionary [subCategoryID];
 
-					mCategoryList.Add( new Category( CategoryName,ImagePath,categoryID:subCategoryID ) );
+					mCategoryList.Add (new Category (CategoryName, ImagePath, categoryID: subCategoryID));
 				}				
+			} else {
+				mCategoryList.Add (mCategory);	
 			}
 		}
-
-		List<Product> PopulateProducts( string CategoryID )
-		{
-			List<Product> product = new List<Product> ();
-
-			if (ProductModel.mProductCategoryIDDictionary.ContainsKey (CategoryID)) {
-				foreach (string productID in ProductModel.mProductCategoryIDDictionary[CategoryID]) {
-					string ImagePath = ProductModel.mRootFolderPath + "/" + ParseConstants.IMAGE_FOLDER_NAME + "/" + ProductModel.mProductImageNameDictionary [productID] + ".jpg";
-					string ProductName = ProductModel.mProductNameDictionary [productID];
-					double price = ProductModel.mProductPriceDictionary [productID];
-					string quantity = ProductModel.mProductQuantityDictionary [productID];
-					product.Add (new Product (productID, ProductName, ImagePath, price, quantity)); 
-				}
-			}
-
-			return product;
-		}
-
-
 	}
 }
 
--- bluemart/MainViews/BrowseProductsPage.xaml.cs
+++ bluemart/MainViews/BrowseProductsPage.xaml.cs
@@ -4,26 +4,34 @@ using bluemart.Common.Utilities;
 using bluemart.Common.Objects;
 using bluemart.Common.ViewCells;
 using Xamarin.Forms;
+using System.Linq;
 
 namespace bluemart.MainViews
 {
 	public partial class BrowseProductsPage : ContentPage
 	{
-		private List<Product> mProducts;
+		private Dictionary<string,List<Product>> mProductDictionary;
 		private int mRowCount;
 		public List<ProductCell> mProductCellList;
+		private List<BoxView> mBoxViewList;
 
-		public BrowseProductsPage (List<Product> product,Category category)
+		public BrowseProductsPage (Dictionary<string,List<Product>> productDictionary,Category category)
 		{						
 			InitializeComponent ();
 			mProductCellList = new List<ProductCell> ();
+			mBoxViewList = new List<BoxView> ();
 			NavigationBar.NavigationText.Text = category.Name;
-			mProducts = product;
-			mRowCount = Convert.ToInt32(Math.Ceiling(mProducts.Count / 2.0f));
+			mProductDictionary = productDictionary;
+			int count = 0;
+			foreach (var product in productDictionary) {
+				count += product.Value.Count;
+			}
+			mRowCount = Convert.ToInt32(Math.Ceiling(count / 2.0f));
 
 			NavigationPage.SetHasNavigationBar (this, false);
 			SetGrid1Definitions ();
 			PopulateGrid ();
+
 		}
 
 		public void  UpdatePriceLabel()
@@ -39,27 +47,107 @@ namespace bluemart.MainViews
 		private void SetGrid1Definitions()
 		{
 			Grid1.RowDefinitions [0].Height = GridLength.Auto;
-			Grid1.RowDefinitions [1].Height = GridLength.Auto;
+			Grid1.RowDefinitions [1].Height = MyDevice.ScreenHeight / 15;
+			Grid1.RowDefinitions [2].Height = GridLength.Auto;
 			Grid1.ColumnDefinitions [0].Width = MyDevice.ScreenWidth;
 			Grid1.BackgroundColor = MyDevice.BlueColor;
+
+			if (mProductDictionary.Count <= 1) {
+				ScrollView1.IsEnabled = false;
+				Grid1.RowDefinitions [1].Height = 0;
+				ScrollView1.IsVisible = false;
+			}
+		}
+
+		private void PopulateSubCategoryButtons()
+		{
+			mBoxViewList.Clear ();
+			foreach (var productPair in mProductDictionary) {
+
+				var relativeLayout = new RelativeLayout(){					
+					VerticalOptions = LayoutOptions.Fill,
+					BackgroundColor = Color.White,
+					Padding = 0
+				};
+
+				Button button = new Button (){
+					VerticalOptions = LayoutOptions.Fill,
+					BackgroundColor = Color.White,
+					Text = productPair.Key,
+					BorderWidth = 0,
+					BorderRadius = 0,
+					TextColor = MyDevice.RedColor,
+					FontSize = Device.GetNamedSize(NamedSize.Small,typeof(Label))
+				};
+
+				BoxView boxView = new BoxView (){
+					HeightRequest = 3,
+					Color = Color.Olive,
+					IsVisible = false
+				};
+				mBoxViewList.Add (boxView);
+				relativeLayout.Children.Add (button,Constraint.RelativeToParent( (parent) =>{
+					return 0;
+				}));
+				relativeLayout.Children.Add(boxView, 
+					Constraint.RelativeToView(button, (parent, sibling) =>
+						{
+							return sibling.Bounds.Left + 5;
+						}),
+					Constraint.RelativeToView(button, (parent, sibling) =>
+						{
+							return sibling.Bounds.Bottom - 3;//sibling.Bounds.Bottom + 5;
+						}),
+					Constraint.RelativeToView(button, (parent, sibling) =>
+						{
+							return sibling.Width - 10;//sibling.Bounds.Bottom + 5;
+						}));
+
+				SubCategoryStackLayout.Children.Add (relativeLayout);
+			}
+
+			mBoxViewList [0].IsVisible = true;
+			//XLabs.Forms.Controls.ExtendedButton.
 		}
 
 		private void PopulateGrid()
 		{
 			SetGrid2Definitions ();
-			int counter = 0;
-			for (int row = 0; row < mRowCount; row++) 
+			PopulateSubCategoryButtons ();
+
+			//Populate a list with all products 
+			//To be able to define product index
+			var valueList = mProductDictionary.Values.Cast<List<Product>> ().ToList();
+			var tempProductList = new List<Product> ();
+			foreach (var products in valueList) {
+				foreach (var tempProduct in products) {
+					tempProductList.Add (tempProduct);	
+				}
+
+			}
+
+			foreach (var productPair in mProductDictionary) {
+				var productList = productPair.Value;
+				foreach (var product in productList) {
+					ProductCell productCell = new ProductCell (Grid2, product, this);
+					int productIndex = tempProductList.IndexOf(product);
+
+					Grid2.Children.Add (productCell.View, productIndex % 2, productIndex / 2);
+				}					
+			}
+
+			/*for (int row = 0; row < mRowCount; row++) 
 			{
 				for (int col = 0; col < 2; col++) 
 				{
-					ProductCell productCell = new ProductCell (Grid2,mProducts[counter++],this );	
+					ProductCell productCell = new ProductCell (Grid2,[counter++],this );	
 					mProductCellList.Add (productCell);
 					Grid2.Children.Add (productCell.View, col, row);
 
 					if ( counter == mProducts.Count)
 						break;
 				}
-			}
+			}*/
 		}
 
 		private void SetGrid2Definitions()
